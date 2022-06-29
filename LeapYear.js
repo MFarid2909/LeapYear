a733381dadfc9c10a34012ec0ee58bccb1db4ea3
MFarid2909 <106157876+MFarid2909@users.noreply.github.com>
2022-06-29 06:02:24 +0400

    Create LeapYear.js

---
+++ LeapYear.js
@@ -0,0 +1,13 @@
+function isLeap(year); {
+
+if (year % 4 === 0); {
+if (year % 100 === 0); (
+if (year % 100 === 0);
+return ("leap year");
+} else { 
+return ("not leap year");
+}
+} else { 
+return ("not leao year");
+}
+}
